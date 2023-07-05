c598fe318239baab222d03fd0afcb4cae2c7818e
bdevops99 <bdevops99@gmail.com>
2023-07-05 18:08:57 +0530

    Docker - Kubernetes

--- rabbitmq.py
+++ rabbitmq.py
@@ -34,7 +34,7 @@ class Publisher:
                                     body=json.dumps(msg).encode())
         self._logger.info('message sent')
 
-    #Publish msg, reconnecting if necessary.
+    #Publish msg,  reconnecting if necessary.
     def publish(self, msg, headers):
         if self._channel is None or self._channel.is_closed or self._conn is None or self._conn.is_closed:
             self._connect()
